393a57a715b2ef4387e098727ee2e73ee43c3b63
djsteve33 <swomble@aol.com>
2021-07-24 11:00:54 -0400

    added data to the seeds file

--- music-backend/db/seeds.rb
+++ music-backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+rock = Genre.create(name: "rock")
+country = Genre.create(name: "country")
+jazz = Genre.create(name: "jazz")
+
+Album.create([
+    {title: 'Pyromania', artist_name: 'Def Leppard', media_type: 'CD', genre: 'rock'},
+    {title: 'Synchronicity', artist_name: 'The Police', media_type: 'cassette', genre: 'rock'},
+    {title: 'No Fences', artist_name: 'Garth Brooks', media_type: 'vinyl', genre: 'country'},
+    {title: 'Popsicle Toes', artist_name: 'Diana Krall', media_type: 'CD', genre: 'jazz'}
+])
